eb4441b6cc865430c1936a646088bdf861efa800
André Pereira <andreluisp@totvs.com.br>
2019-10-25 17:11:21 -0300

    ajustes no abrircontacomando

--- src/main/java/com/totvs/tj/tcc/app/conta/ContaApplicationService.java
+++ src/main/java/com/totvs/tj/tcc/app/conta/ContaApplicationService.java
@@ -5,11 +5,19 @@ import org.springframework.stereotype.Service;
 import com.totvs.tj.tcc.domain.conta.Conta;
 import com.totvs.tj.tcc.domain.conta.ContaId;
 import com.totvs.tj.tcc.domain.conta.ContaRepository;
+import com.totvs.tj.tcc.domain.empresa.Empresa;
+import com.totvs.tj.tcc.domain.empresa.EmpresaId;
+import com.totvs.tj.tcc.domain.empresa.EmpresaRepository;
+import com.totvs.tj.tcc.domain.responsavel.Responsavel;
+import com.totvs.tj.tcc.domain.responsavel.ResponsavelId;
+import com.totvs.tj.tcc.domain.responsavel.ResponsavelRepository;
 
 @Service
 public class ContaApplicationService {
     
     private ContaRepository repository;
+    private EmpresaRepository empresaRepository;
+    private ResponsavelRepository responsavelRepository;
     
     public ContaApplicationService(ContaRepository repository) {
         this.repository = repository;
@@ -21,13 +29,21 @@ public class ContaApplicationService {
         
         Conta conta = Conta.builder()
                 .id(idConta)
-                .empresa(cmd.getEmpresa())
-                .responsavel(cmd.getResponsavel())
+                .empresa(getEmpresaById(cmd.getEmpresaId()))
+                .responsavel(getResponsavelById(cmd.getResponsavelId()))
             .build();
         
         repository.save(conta);
         
         return idConta; 
+    }
+    
+    protected Empresa getEmpresaById(EmpresaId empresaId) {
+        return empresaRepository.getOne(empresaId);
+    }
+    
+    protected Responsavel getResponsavelById(ResponsavelId responsavelId) {
+        return responsavelRepository.getOne(responsavelId);
     }
     
     public void handle(SuspenderContaCommand cmd) {
