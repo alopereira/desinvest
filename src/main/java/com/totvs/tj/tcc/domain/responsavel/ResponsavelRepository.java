a9d6d05751dfffe68cbf358d6aaf863202cc4ea2
aa <aaa>
2019-10-30 14:21:14 -0300

    ajustes nos repositorios

--- src/main/java/com/totvs/tj/tcc/domain/responsavel/ResponsavelRepository.java
+++ src/main/java/com/totvs/tj/tcc/domain/responsavel/ResponsavelRepository.java
@@ -2,7 +2,7 @@ package com.totvs.tj.tcc.domain.responsavel;
 
 public interface ResponsavelRepository {
 
-    void save(Responsavel conta);
+    void save(Responsavel responsavel);
     
     Responsavel getOne(ResponsavelId id);
     
--- src/main/java/com/totvs/tj/tcc/infra/persistence/ResponsavelRepositoryJpa.java
+++ src/main/java/com/totvs/tj/tcc/infra/persistence/ResponsavelRepositoryJpa.java
@@ -1,13 +1,13 @@
 package com.totvs.tj.tcc.infra.persistence;
 
+import com.totvs.tj.tcc.domain.responsavel.Responsavel;
+import com.totvs.tj.tcc.domain.responsavel.ResponsavelId;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.totvs.tj.tcc.domain.empresa.Empresa;
-import com.totvs.tj.tcc.domain.empresa.EmpresaId;
 import com.totvs.tj.tcc.domain.responsavel.ResponsavelRepository;
 
 @Repository
-public interface ResponsavelRepositoryJpa extends ResponsavelRepository, JpaRepository<Empresa, EmpresaId> {
+public interface ResponsavelRepositoryJpa extends ResponsavelRepository, JpaRepository<Responsavel, ResponsavelId> {
 
 }
