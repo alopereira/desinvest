087652073d33772ce1870ba05c380d33e5c50dfc
aa <aaa>
2019-11-04 16:14:45 -0300

    Revert "Merge branch 'master' into versao_nova_depois_do_tiago"
    This reverts commit 33cd426ce57a514bb7eb49cbb924b664d214fdb9, reversing
    changes made to efe012776fbe76907c1e7438ce17f1094d0251c6.

--- src/main/java/com/totvs/tj/tcc/domain/conta/Conta.java
+++ src/main/java/com/totvs/tj/tcc/domain/conta/Conta.java
@@ -1,17 +1,5 @@
 package com.totvs.tj.tcc.domain.conta;
 
-import static com.totvs.tj.tcc.domain.conta.Conta.Situacao.ABERTO;
-import static com.totvs.tj.tcc.domain.conta.Conta.Situacao.SUSPENSO;
-
-import com.totvs.tj.tcc.domain.empresa.Empresa;
-import com.totvs.tj.tcc.domain.empresa.EmpresaId;
-import com.totvs.tj.tcc.domain.movimentacao.Movimentacao;
-import com.totvs.tj.tcc.domain.movimentacao.MovimentacaoId;
-import com.totvs.tj.tcc.domain.movimentacao.MovimentacaoMotivoRecusa;
-import com.totvs.tj.tcc.domain.movimentacao.MovimentacaoSituacao;
-import com.totvs.tj.tcc.domain.responsavel.Responsavel;
-import com.totvs.tj.tcc.domain.responsavel.ResponsavelId;
-
 import lombok.Builder;
 import lombok.Getter;
 import lombok.ToString;
@@ -22,13 +10,7 @@ import lombok.ToString;
 public class Conta {
 
     private ContaId id;
-
-    private Empresa empresa;
-
-    private Responsavel responsavel;
-
-    private Situacao situacao;
-
+    
     private double saldo;
 
     private double limite;    
